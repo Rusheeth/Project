0bf83b198a9e2163dc1b14677096dfa85f4a2a1a
Rusheeth <136624130+Rusheeth@users.noreply.github.com>
2025-10-17 10:08:56 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,16 @@
+# train_model.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load dataset
+X, y = load_iris(return_X_y=True)
+
+# Train simple model
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, "model.pkl")
+
+print("✅ Model trained and saved as model.pkl")
